cb64a792ef22f2333b4dfb7a9432a67778f2175a
Nazmul Alam <nazmuldipu@gmail.com>
2020-02-19 18:51:52 +0600

    basic components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+// routes
+export const ROUTES: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: '../dashboard/dashboard.module#DashboardModule'
+  },
+  {
+    path: '',
+    loadChildren: '../home/home.module#HomeModule'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/home/home.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { SharedModule } from '../shared/shared.module';
+import { IndexComponent } from './containers/index/index.component';
+import { LoginComponent } from './containers/login/login.component';
+import { RegisterComponent } from './containers/register/register.component';
+import { HomeComponent } from './home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+export const ROUTES: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: IndexComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [HomeComponent, IndexComponent, LoginComponent, RegisterComponent, NavbarComponent],
+  imports: [
+    SharedModule, RouterModule.forChild(ROUTES)
+  ]
+})
+export class HomeModule { }
